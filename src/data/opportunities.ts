c004c7a96e87ca23f5e091835936aa2fa0a78aa2
monoguitari <jesus_rodriguez@brown.edu>
2025-01-01 07:05:33 -0500

    feat: experience content and styling

---
+++ src/data/opportunities.ts
@@ -0,0 +1,50 @@
+export const opportunities = [
+  {
+    dates: "2024 - Present",
+    title: "Software Engineer II",
+    company: "Walmart",
+    technology: [],
+    description: "ipsum loram",
+  },
+  {
+    dates: "June 2024 - August 2024",
+    title: "Data Science Intern",
+    company: "Center for Computation and Visualization",
+    technology: ["Docker", "NVIDIA Parabricks", "GPU Clusters"],
+    description:
+      "Developed and deployed a Docker container for NVIDIA's Parabricks framework on a high-performance computing cluster. Enhanced code performance by leveraging GPU clusters, reducing runtime from 3 hours to 30 minutes.",
+  },
+  {
+    dates: "March 2024 - Present",
+    title: "Machine Learning Engineer",
+    company: "Embrace Medical",
+    technology: ["React", "Machine Learning frameworks", "Python"],
+    description:
+      "Developing a machine learning model for medical prosthesis through advanced data analysis and algorithm optimization. Designed and implemented a React-based website to improve user experience and mobile accessibility.",
+  },
+  {
+    dates: "March 2024 - Present",
+    title: "Science Communicator and Web Designer/Developer",
+    company: "Serre Lab",
+    technology: ["React", "Figma", "Neuro AI tools"],
+    description:
+      "Redesigned and implemented an outdated lab website using Figma and React, enhancing user experience and visual appeal. Created engaging educational content on neuro AI for social media, simplifying complex concepts for a broad audience.",
+  },
+  {
+    dates: "February 2024 - Present",
+    title: "Software Engineer",
+    company: "Full Stack at Brown",
+    technology: ["React", "TypeScript", "GraphQL"],
+    description:
+      "Developed a GraphQL database integrated with a content management system. Built the client's website from scratch using React and TypeScript, translating designers' visions into functional components.",
+  },
+  {
+    dates: "July 2022 - August 2022",
+    title: "Researcher",
+    company: "OppNet Summer Science Research Program",
+    technology: [],
+    description:
+      "Conducted in-depth research on music therapy as an intervention for individuals with disabilities. Developed a comprehensive literature review on advancements in music therapy and disability studies to support academic research.",
+  },
+];
+
