97fc400ba4f74169c37d56c4f5ffe29835ad9941
monoguitari <jesus_rodriguez@brown.edu>
2025-01-01 18:00:51 -0500

    feat: project structure

---
+++ src/data/projects.ts
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    title: "Integrating Neural Networks with Linear Regression to Enhance Control Vectors in Social Sciences",
+    desc: "This project explores the integration of neural network-based feature extraction with traditional linear regression models to improve prediction accuracy in the social sciences. This approach aims to maintain the interpretability of linear models while leveraging the predictive power of neural networks to enhance feature representation. This method could be particularly useful in fields where the interaction effects of variables are significant but difficult to model linearly.",
+    github: "https://github.com/monoguitari/social-science-dl-final",
+    tech: ["Python", "TensorFlow"],
+    image: "social-science-dl.png"
+  }
+]
